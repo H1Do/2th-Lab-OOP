8e88c92ad91f0c7d696fb6c5c979922068d6dfeb
H1Do <90624145+H1Do@users.noreply.github.com>
2023-03-13 20:30:14 +0500

    Добавьте файлы проекта.

---
+++ 2llab.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Point {
+public:
+    int x, y;
+public:
+    Point() : x(0), y(0) {
+        std::cout << "Point()\n";
+    }
+    Point(int x, int y) : x(x), y(y) {
+        std::cout << "Point(int x, int y)\n";
+    }
+    Point(const Point& p) : x(p.x), y(p.y) {
+        std::cout << "Point(const Point& p)\n";
+    }
+    ~Point() {
+        std::cout << x << ' ' << y << " ~Point()\n";
+    }
+    int getX() {
+        return x;
+    }
+    int getY();
+};
+
+int Point::getY() {
+    return y;
+}
+
+int main() {
+    {
+        Point a();
+        Point b(10, 10);
+        Point c(b);
+    }
+}
